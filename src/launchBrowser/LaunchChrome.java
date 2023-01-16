c4fbbcc4d11bf8dff7cb66f533e4336405ec168d
Qurratulain11 <qurratulain_flohr@yahoo.com>
2023-01-16 01:08:39 -0500

    commit

---
+++ src/launchBrowser/LaunchChrome.java
@@ -0,0 +1,13 @@
+package launchBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) {
+
+		// System.setProperty("webdriver.chrome.driver","C:\\Users\\mumta\\OneDrive\\Desktop\\Selenium\\firstt_Selenium\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	}
+}
